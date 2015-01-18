179190177613144a79874c424f2f19bdeb321038
marcolunardi <marco@marcolunardi.com>
2015-01-18 02:20:23 +0000

    Commit 04
    Some minor text corrections added

--- ui.R
+++ ui.R
@@ -10,17 +10,17 @@ shinyUI(pageWithSidebar(
         mainPanel(
                 h3("App Documentation (read me first, please):"),
                 p("This simple app is designed to perform a very basic exploratory analysis
-                  upon the mtcars dataset included into the dataset package in R."),
-                p("You can find more info about this dataset by typing 'library(dataset)' and '? mtcars' at R prompt."),
-                p("When you modify the settings of engine cylinders number and transmission type,
-                  the app changes the below title content, computes the mean of MPGs and weights for cars corresponding to the chosen settings,
+                  upon the mtcars dataset included into the 'datasets' package in R."),
+                p("You can find more info about this dataset by typing 'library(datasets)' and '? mtcars' at the prompt of R console."),
+                p("When you modify the settings of engine cylinders number and transmission type on the upper left side of this page,
+                  then the app changes the below title content, computes the mean of MPGs and weights for cars corresponding to the chosen settings,
                   and plots a graph."),
-                p("The graph reports just the dataset instances corresponding to the chosen settings, so it's
-                  possible to compare them with the average values drawn from the whole dataset (the two fixed orange lines
+                p("The graph reports just the cars corresponding to the chosen settings, so it's
+                  possible to compare their MPG and weight values with the average values drawn from the whole dataset (the two fixed orange lines
                   which split the graph in four quadrants)."),
                 p("The reason why I chose to plot the MPG consumption against the weight is that the weight
                   has quite a direct influence on fuel consumption, and so it's necessary to take it into account while
-                  comparing the MPGs from different cars."),
+                  comparing the MPG values of different cars."),
                 p("For a correct evaluation, please note that a higher MPG means a lower fuel consumption (more miles with one gallon),
                   so the upper two quadrants do contain the more efficient cars."),                
                 h3(textOutput("title")),
