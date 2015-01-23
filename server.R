8065bf01eb912a5609bffa4f31f1b9ab476ed8cb
marcolunardi <marco@marcolunardi.com>
2015-01-23 01:22:31 +0000

    Commit 06
    Minor modification to the interactive graph

--- server.R
+++ server.R
@@ -31,7 +31,7 @@ shinyServer(function(input, output) {
                 plot(cars$wt, cars$mpg, col=cars$am, xlim=c(1,6), ylim=c(10,40), 
                      ylab="MPG = Miles per Gallon", xlab="Weight (lbs/1000)", cex.main=1,
                      main="MPG by Weight; Orange Lines =  mean weight and MPG for all cars",
-                     pch=16, cex=3)
+                     pch=16, cex=2)
                 abline(h = mpgmean, col="orange", lwd=4)
                 abline(v = wtmean, col="orange", lwd=4)
         })
